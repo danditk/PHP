e197a569252de6f65763812e822b947a7ebb4d9c
danditk <danditkaczuk@gmail.com>
2020-05-05 19:38:04 +0200

    Byg fix, variables mistake id name - result vs rezult

--- PHP_L2/www/Osadnicy/zaloguj.php
+++ PHP_L2/www/Osadnicy/zaloguj.php
@@ -5,13 +5,28 @@
 	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 
 	if($polaczenie->connect_errno!=0){
-		echo "Error: ".$polaczenie->connect_errno. " " . "Opis: ".$polaczenie->connect_error;
+		echo "Error: ".$polaczenie->connect_errno . " " . "Opis: ".$polaczenie->connect_error;
+		// . " " . "Opis: ".$polaczenie->connect_error
 	}
 	else {
 		$login = $_POST['login'];
 		$password = $_POST['password'];
 
-		echo "It works!";
+		$sql = "SELECT * FROM uzytkownicy WHERE user='$login' AND pass='$password'";
+
+		if($rezultat = @$polaczenie->query($sql)){
+			$ilu_userow = $rezultat->num_rows;
+			if($ilu_userow>0) {
+				$row = $rezultat->fetch_assoc();
+				$user = $row['user'];
+
+				echo $user;
+
+				$rezultat->close();
+			} else {
+				// code...
+			}
+		}
 
 		$polaczenie->close();
 	}
