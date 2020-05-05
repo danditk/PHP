b432e531fb9b66445998a63e20f84a99cd00ca84
danditk <danditkaczuk@gmail.com>
2020-05-05 15:19:23 +0200

    DB connection works

---
+++ PHP_L2/www/Osadnicy/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Osadnicy - gra przeglądarkowa</title>
+	<meta name="autor" content="lesserr" />
+	<meta name="description" content="Opis witryny" />
+	<meta name="keywords" content="słowa kluczowe" />
+	<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+  <h1>Tylko martwi ujrzeli koniec wojny - Platon</h1><br><br>
+
+  <form class="" action="zaloguj.php" method="post">
+    Login: <br> <input type="text" name="login"><br><br>
+    Hasło: <br> <input type="password" name="password"><br><br>
+    <input type="submit" value="Zaloguj">
+  </form>
+
+
+
+</body>
+
+</html>
---
+++ PHP_L2/www/Osadnicy/zaloguj.php
@@ -0,0 +1,19 @@
+<?php
+
+	require_once "connect.php";
+
+	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+
+	if($polaczenie->connect_errno!=0){
+		echo "Error: ".$polaczenie->connect_errno. " " . "Opis: ".$polaczenie->connect_error;
+	}
+	else {
+		$login = $_POST['login'];
+		$password = $_POST['password'];
+
+		echo "It works!";
+
+		$polaczenie->close();
+	}
+
+ ?>
